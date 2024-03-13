a79dc069d6addafe3e3cc7a5f348115e6e6a22a2
clymber <32444460+clymber@users.noreply.github.com>
2024-03-13 23:44:28 +0800

    [NEW]to_integral() support unsigned types and skip leading white spaces

--- gtestC/str_to_numeric.h
+++ gtestC/str_to_numeric.h
@@ -4,7 +4,7 @@
 #include <type_traits>
 
 template<typename T>
-constexpr T integral_max(void) {
+constexpr T max_integral_value(void) {
     static_assert(std::is_integral<T>::value, "Only integral types allowed.");
     if constexpr(std::is_signed<T>::value) {
         return T(~(T(0x01) << ((sizeof(T) << 3) - 1)));
@@ -14,58 +14,40 @@ constexpr T integral_max(void) {
 }
 
 template<typename T>
-T to_positive_integral(const std::string& str) {
-    static_assert(std::is_integral<T>::value, "Only integral types allowed.");
-    constexpr T MAX_INT = integral_max<T>();
-    T digit{0}, result{0};
-
-    for (std::string::size_type i{0}; i < str.size(); ++i) {
-        digit = str[i] - '0';
-        if (digit < 0 or digit > 9) {
-            break;
-        }
-
-        if (digit > MAX_INT - result*10) {
-            break;
-        }
-
-        result = 10 * result + digit;
-        if (result > MAX_INT/10) {
-            break;
-        }
-    }
-    return result;
+constexpr size_t max_decimal_digits(void) {
+    size_t n_digit{0};
+    for (T temp{max_integral_value<T>()}; temp > 0; temp /= 10, ++n_digit);
+    return n_digit;
 }
 
 template<typename T>
 T to_integral(const std::string& str) {
-    static_assert(std::is_integral<T>::value, "Only integral types allowed.");
-    constexpr T MAX_INT = integral_max<T>();
-    constexpr int MAX_DIGITS = [=]{
-        int n_digit{0};
-        for (T temp{MAX_INT}; temp > 0; temp /= 10, ++n_digit);
-        return n_digit;
-    }();
-
+    constexpr T MAX_INT = max_integral_value<T>();
+    constexpr size_t MAX_DIGITS = max_decimal_digits<T>();
     T digit{0}, result{0}, sign{1};
     std::string::size_type i{0};
 
-    if (str[i] == '-') {
-        if constexpr(! std::is_signed<T>::value) {
-            return 0;
-        } else {
+    // skip leading spaces
+    while (str[i] == ' ' or str[i] == '\n' or str[i] == '\t' or str[i] == '\v'){
+        ++i;
+    }
+
+    // support negative value for signed types only
+    if constexpr(std::is_signed<T>::value) {
+        if (str[i] == '-') {
             sign = -1;
             ++i;
         }
     }
 
-    for (int ndigit{1}; i < str.size(); ++i, ++ndigit) {
+    // process the decimal characters
+    for (size_t ndigit{0}; i < str.size(); ++i) {
         digit = str[i] - '0';
         if (digit < 0 or digit > 9) {
             break;
         }
 
-        if (ndigit < MAX_DIGITS) {
+        if (++ndigit < MAX_DIGITS) {
             result = 10 * result + digit;
             continue;
         }
--- gtestC/str_to_numeric_gtest.cpp
+++ gtestC/str_to_numeric_gtest.cpp
@@ -5,21 +5,33 @@
 
 using namespace std;
 
-class TestStr2Int: public testing::Test {};
+class TestStrToIntegral: public testing::Test {};
 
-TEST_F(TestStr2Int, PositiveIntegerOverflowIssues) {
-    ASSERT_EQ(to_positive_integral<int>("2147483646"s), 2147483646);
-    ASSERT_EQ(to_positive_integral<int>("2147483647"s), 2147483647);
-    ASSERT_EQ(to_positive_integral<int>("2147483648"s), 214748364);
-    ASSERT_EQ(to_positive_integral<char>("126"s), 126);
-    ASSERT_EQ(to_positive_integral<char>("127"s), 127);
-    ASSERT_EQ(to_positive_integral<char>("128"s), 12);
-
-    // compile error's expected:
-    // ASSERT_EQ(to_positive_integral<double>("123"s), 123);
+TEST_F(TestStrToIntegral, SkipLeadingWhiteSpaces) {
+    ASSERT_EQ(to_integral<int8_t>(" 123"), 123);
+    ASSERT_EQ(to_integral<int16_t>("\n123"), 123);
+    ASSERT_EQ(to_integral<int32_t>("\t123"), 123);
+    ASSERT_EQ(to_integral<int64_t>("\v123"), 123);
+    ASSERT_EQ(to_integral<uint32_t>(" \n\t\v123"), 123);
 }
 
-TEST_F(TestStr2Int, NegativeIntegerOverflow) {
+TEST_F(TestStrToIntegral, SignedTypesSupportNegative) {
+    ASSERT_EQ(to_integral<int8_t>(" -123"), -123);
+    ASSERT_EQ(to_integral<int16_t>("\n-123"), -123);
+    ASSERT_EQ(to_integral<int32_t>("\t-123"), -123);
+    ASSERT_EQ(to_integral<int64_t>("\v-123"), -123);
+    ASSERT_EQ(to_integral<int32_t>(" \n\t\v-123"), -123);
+}
+
+TEST_F(TestStrToIntegral, UnsignedTypesNotSupportNegative) {
+    ASSERT_EQ(to_integral<uint8_t>(" -123"), 0);
+    ASSERT_EQ(to_integral<uint16_t>("\n-123"), 0);
+    ASSERT_EQ(to_integral<uint32_t>("\t-123"), 0);
+    ASSERT_EQ(to_integral<uint64_t>("\v-123"), 0);
+    ASSERT_EQ(to_integral<uint32_t>(" \n\t\v-123"), 0);
+}
+
+TEST_F(TestStrToIntegral, InCasesOfOverflow) {
     ASSERT_EQ(to_integral<int64_t>("9223372036854775806"s), 9223372036854775806);
     ASSERT_EQ(to_integral<int64_t>("9223372036854775807"s), 9223372036854775807);
     ASSERT_EQ(to_integral<int64_t>("9223372036854775808"s), 922337203685477580);
@@ -45,7 +57,6 @@ TEST_F(TestStr2Int, NegativeIntegerOverflow) {
     ASSERT_EQ(to_integral<int8_t>("-128"s), -128);
     ASSERT_EQ(to_integral<int8_t>("-129"s), -12);
 
-    ASSERT_EQ(to_integral<uint32_t>("-123"s), 0);
     ASSERT_EQ(to_integral<uint32_t>("4294967294"s), 0xFFFFFFFE);
     ASSERT_EQ(to_integral<uint32_t>("4294967295"s), 0xFFFFFFFF);
     ASSERT_EQ(to_integral<uint32_t>("4294967296"s), 429496729);
